7d1b1ea96a4cc14371c27b28f6c643241120dd5d
Nicolas Taccari <nicolastaccari@gmail.com>
2020-03-22 18:51:08 -0300

    typewrite effect with hooks

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,21 +1,17 @@
 import React from "react";
+import { Typewrite } from "../utils/typewrite";
 
 export const Slider = () => {
+  const messages = ["PRODUCE", "DESIGN", "RECORD", "CREATE"];
   return (
     <section class="home-slider ftco-degree-bg">
       <div class="slider-item">
         <div class="container">
           <div class="row slider-text align-items-center justify-content-center">
-            <div class="col-md-10 ftco-animate text-center">
+            <div class="col-md-10 text-center">
               <h1 class="mb-4">
                 I love to
-                <strong
-                  class="typewrite"
-                  data-period="4000"
-                  data-type='[ "Produce.", "Design.", "Explore.", "Capture." ]'
-                >
-                  <span class="wrap"></span>
-                </strong>
+                <Typewrite heading={"We love to:"} messages={messages} />
               </h1>
               <p>
                 A small river named Duden flows by their place and supplies it
