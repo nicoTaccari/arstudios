bb6a8fe35004edc89030941ac7abeed06ab50003
Nicolas Taccari <nicolastaccari@gmail.com>
2020-04-09 13:55:07 -0300

    api key fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ const withAssetsImport = require("next-assets-import");
 module.exports = withAssetsImport({
   env: {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    API_KEY_YOUTUBE: process.env.YOUTUBE_API_KEY,
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
   },
 });
