ce851a4017c6f9ce797a1427b0920bf74ea4b5d1
Nicolas Taccari <nicolastaccari@gmail.com>
2020-04-09 13:59:40 -0300

    fix youtube api key

--- constants.js
+++ constants.js
@@ -1,5 +1,4 @@
 export const RESULTS = 9;
 
-export const YOUTUBE_URL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.API_KEY_YOUTUBE}&channelId=${process.env.CHANNEL_ID}&part=snippet,id&order=date&maxResults=${RESULTS}`;
-export const CALENDAR_URL =
-  "https://calendar.google.com/calendar?cid=${process.env.CHANNEL_ID}";
+export const YOUTUBE_URL = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${process.env.CHANNEL_ID}&part=snippet,id&order=date&maxResults=${RESULTS}`;
+export const CALENDAR_URL = `https://calendar.google.com/calendar?cid=${process.env.CHANNEL_ID}`;
