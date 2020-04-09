ad65d080db6ea1bcafc011686131b79cd06cd0b9
Nicolas Taccari <nicolastaccari@gmail.com>
2020-04-09 16:19:40 -0300

    change key

--- components/contact/form/index.js
+++ components/contact/form/index.js
@@ -63,21 +63,6 @@ const ContactForm = () => {
     <form onSubmit={handleOnSubmit} action="#" className="p-5 bg-white">
       <h2 className="h4 text-black mb-5">Contact Form</h2>
 
-      {/* <div className="row form-group">
-        <div className="col-md-6 mb-3 mb-md-0">
-          <label className="text-black" for="fname">
-            First Name
-          </label>
-          <input type="text" id="fname" className="form-control" />
-        </div>
-        <div className="col-md-6">
-          <label className="text-black" htmlFor="lname">
-            Last Name
-          </label>
-          <input type="text" id="lname" className="form-control" />
-        </div>
-      </div> */}
-
       <div className="row form-group">
         <div className="col-md-12">
           <label className="text-black" htmlFor="email">
