4660af1964888277d6bcb0ce84de36ec850d907c
Nicolas Taccari <nicolastaccari@WKSAR286L.local>
2020-04-16 18:30:04 -0300

    calendar

--- components/calendar/index.js
+++ components/calendar/index.js
@@ -10,6 +10,14 @@ const Calendar = () => {
     <section className="section bg-light" id="calendar">
       <div className="container">
         <SectionHeader title={title} subtitle={subtitle} />
+        <iframe
+          src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23B39DDB&amp;ctz=America%2FArgentina%2FBuenos_Aires&amp;src=YXByaWxyb2Fkc3R1ZGlvc0BnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%234285F4&amp;color=%237986CB&amp;showCalendars=0&amp;showPrint=0&amp;showTitle=0"
+          // style={{"border:solid 1px #777"}}
+          width="800"
+          height="600"
+          frameborder="0"
+          scrolling="no"
+        ></iframe>
       </div>
     </section>
   );
