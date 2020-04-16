682b4233ba0957cfdc1587a4da524a4fdb439446
Nicolas Taccari <nicolastaccari@gmail.com>
2020-04-16 20:20:04 -0300

    address icons

--- components/contact/address/index.js
+++ components/contact/address/index.js
@@ -4,16 +4,23 @@ const Address = () => {
   return (
     <div className="col-md-5">
       <div className="p-4 mb-3 bg-white">
-        <p className="mb-0 font-weight-bold">Address</p>
-        <p className="mb-4">
-          203 Fake St. Mountain View, San Francisco, California, USA
-        </p>
+        <address>
+          <p className="mb-0 font-weight-bold">April Road Studios</p>
+          <p className="mb-4">
+            <i class="fas fa-map-marker-alt mr-2"></i>
+            203 Fake St. Mountain View, San Francisco, California, USA
+          </p>
 
-        <p className="mb-0 font-weight-bold">Phone</p>
-        <p className="mb-4">+1 232 3235 324</p>
+          <p className="mb-0 font-weight-bold">Phone</p>
+          <p className="mb-4">
+            <i class="fas fa-phone mr-2"></i>+1 232 3235 324
+          </p>
 
-        <p className="mb-0 font-weight-bold">Email Address</p>
-        <p className="mb-0">arstudios@gmail.com</p>
+          <p className="mb-0 font-weight-bold">Email Address</p>
+          <p className="mb-0">
+            <i class="fas fa-envelope mr-2"></i>arstudios@gmail.com
+          </p>
+        </address>
       </div>
     </div>
   );
