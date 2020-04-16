4f3d5aa85393042f4ba597d4c5f225acab618ac2
Nicolas Taccari <nicolastaccari@gmail.com>
2020-04-16 20:23:08 -0300

    deleted calendar key

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = withAssetsImport({
   env: {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
-    CALENDAR_API_KEY: process.env.CALENDAR_API_KEY,
     YOUTUBE_CHANNEL_ID: process.env.YOUTUBE_CHANNEL_ID,
     MAPGL_API_KEY: process.env.MAPGL_API_KEY,
   },
