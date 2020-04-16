8be7c71ea75d9c67e601456aad660134d4712c01
Nicolas Taccari <nicolastaccari@WKSAR286L.local>
2020-04-16 18:48:28 -0300

    calendar and about

--- components/about/index.js
+++ components/about/index.js
@@ -4,8 +4,14 @@ import SectionHeader from "../../common/section-header";
 const About = () => {
   const title = "About";
   const subtitle =
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi unde impedit, necessitatibus, soluta sit quam minima expedita atque corrupti reiciendis.";
-  return (
+    "Bienvenido al lugar donde se reúne la cultura y el arte.\
+    Artspace Barcelona es el sitio ideal para músicos, fotógrafos y todos los profesionales del mundo audiovisual. Estamos ubicados en el centro de Barcelona, calle madrazo 54 esquina con calle Brusi.\
+    Nuestra estrategia es proporcionar el mejor servicio y la mejor \
+    calidad con el mejor ambiente a nuestros clientes en todas sus ideas creativas,\
+    desde su producción hasta incluso descubriendo nuevos talentos, para así contribuir\
+    con la riqueza artística de nuestra generación.";
+  
+    return (
     <section className="section bg-light" id="about">
       <SectionHeader title={title} subtitle={subtitle} />
     </section>
--- components/calendar/index.js
+++ components/calendar/index.js
@@ -3,6 +3,9 @@ import React from "react";
 import SectionHeader from "../../common/section-header";
 
 const Calendar = () => {
+  const calendarStyle = {
+    borderWidth: 0,
+  };
   const title = "Check when to book a session";
   const subtitle =
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi unde impedit, necessitatibus, soluta sit quam minima expedita  atque corrupti reiciendis.";
@@ -11,9 +14,9 @@ const Calendar = () => {
       <div className="container">
         <SectionHeader title={title} subtitle={subtitle} />
         <iframe
-          src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23B39DDB&amp;ctz=America%2FArgentina%2FBuenos_Aires&amp;src=YXByaWxyb2Fkc3R1ZGlvc0BnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%234285F4&amp;color=%237986CB&amp;showCalendars=0&amp;showPrint=0&amp;showTitle=0"
-          // style={{"border:solid 1px #777"}}
-          width="800"
+          src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FArgentina%2FBuenos_Aires&amp;src=YXByaWxyb2Fkc3R1ZGlvc0BnbWFpbC5jb20&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%234285F4&amp;color=%237986CB&amp;showCalendars=0&amp;showPrint=0&amp;showTitle=0&amp;showDate=0&amp;showNav=0&amp;showTz=0"
+          style={calendarStyle}
+          width="100%"
           height="600"
           frameborder="0"
           scrolling="no"
