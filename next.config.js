144ba112c06c590f5c648b62762a022d1fc06c4d
Nicolas Taccari <nicolastaccari@gmail.com>
2020-05-01 17:04:26 -0300

    PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+const withPWA = require("next-pwa");
 const withAssetsImport = require("next-assets-import");
-module.exports = withAssetsImport({
-  env: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
-    YOUTUBE_CHANNEL_ID: process.env.YOUTUBE_CHANNEL_ID,
-    MAPGL_API_KEY: process.env.MAPGL_API_KEY,
-  },
-});
+
+module.exports = withPWA(
+  withAssetsImport({
+    env: {
+      SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+      YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+      YOUTUBE_CHANNEL_ID: process.env.YOUTUBE_CHANNEL_ID,
+      MAPGL_API_KEY: process.env.MAPGL_API_KEY,
+    },
+    pwa: {
+      dest: "public",
+    },
+  })
+);
--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,31 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charset="utf-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+          <title>April Road Studios</title>
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="android-touch-icon" href="/android-icon-36x36.png"></link>
+          <meta name="theme-color" content="#317EFB" />
           <link
             href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
             rel="stylesheet"
